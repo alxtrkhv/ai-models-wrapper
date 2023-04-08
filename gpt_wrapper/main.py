3771aad97e2c8a730d61ac79ade963b2e3a75c00
Alexander Terekhov <alxtrkhv@gmail.com>
2023-04-09 01:21:15 +0300

    refactor: drop direct rich usage

--- gpt_wrapper/main.py
+++ gpt_wrapper/main.py
@@ -1,5 +1,4 @@
-from typer import Typer
-from rich.prompt import Prompt
+from typer import Typer, Option
 from keyring import get_password, set_password, delete_password
 
 import config
@@ -8,20 +7,17 @@ app = Typer(pretty_exceptions_show_locals=False)
 
 
 @app.command("login")
-def login():
-    org_name = Prompt.ask("Enter organisation name")
-    token = Prompt.ask("Enter token", password=True)
-
-    config.update_config({"org_name": org_name})
-
-    set_password(config.APP_NAME, org_name, token)
+def login(
+    organization_id: str = Option(..., prompt=True),
+    token: str = Option(..., prompt=True, hide_input=True),
+):
+    config.update_config({"org_name": organization_id})
+    set_password(config.APP_NAME, organization_id, token)
 
 
 @app.command("logout")
-def logout():
-    should_logout = Prompt.ask("Are you sure you want to logout?", choices=["y", "n"])
-
-    if should_logout == "y":
+def logout(are_you_sure: bool = Option(..., prompt=True)):
+    if are_you_sure:
         org_name = config.read_config().org_name
 
         if org_name is not None:
