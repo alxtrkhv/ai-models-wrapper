386fee9a2e2e47f758feb07df693ca2e6b7d5314
Alexander Terekhov <alxtrkhv@gmail.com>
2023-04-09 00:56:05 +0300

    feat: implement login and logout

--- gpt_wrapper/config.py
+++ gpt_wrapper/config.py
@@ -1,23 +1,54 @@
-from os import open
+from os import path, makedirs
 from json import load, dump
+from pathlib import Path
 
 from pydantic import BaseModel
 
 APP_NAME = "com.alxtrkhv.gpt_wrapper"
-FILE_PATH = f"~/.config/{APP_NAME}"
+FILE_PATH = Path.home() / ".config" / APP_NAME / "config.json"
 
 
-class AppConfig(BaseModel):
-    org_name: str
+class Config(BaseModel):
+    org_name: str | None
 
 
-with open(FILE_PATH, "r") as file:
-    app_config = load(file)
+def read_config() -> Config:
+    _ensure_directory_exists(FILE_PATH)
+
+    try:
+        with open(FILE_PATH, "r") as file:
+            data = load(file)
+    except FileNotFoundError:
+        return Config()
+    except Exception as e:
+        print(f"Error reading config file '{FILE_PATH}': {e}")
+        return Config()
+
+    config = Config(**data)
+    return config
 
 
-def update(new_values: dict):
-    values = app_config.dict()
-    values.update(new_values)
+def update_config(updated_data: dict | Config) -> bool:
+    if isinstance(updated_data, Config):
+        updated_data = updated_data.dict()
 
-    with open(FILE_PATH, "w") as file:
-        dump(values, file)
+    current_config = read_config()
+
+    current_config_dict = current_config.dict()
+    current_config_dict.update(updated_data)
+    updated_config = Config(**current_config_dict)
+
+    try:
+        with open(FILE_PATH, "a+") as file:
+            dump(updated_config.dict(), file, indent=2)
+    except Exception as e:
+        print(f"Error updating config file '{FILE_PATH}': {e}")
+        return False
+
+    return True
+
+
+def _ensure_directory_exists(file_path: str) -> None:
+    dir_name = path.dirname(file_path)
+    if dir_name and not path.exists(dir_name):
+        makedirs(dir_name)
--- gpt_wrapper/main.py
+++ gpt_wrapper/main.py
@@ -1,1 +1,31 @@
-import openai
+from typer import Typer
+from rich.prompt import Prompt
+from keyring import get_password, set_password, delete_password
+
+import config
+
+app = Typer()
+
+
+@app.command("login")
+def login():
+    org_name = Prompt.ask("Enter organisation name")
+    token = Prompt.ask("Enter token", password=True)
+
+    config.update_config({"org_name": org_name})
+
+    set_password(config.APP_NAME, org_name, token)
+
+
+@app.command("logout")
+def logout():
+    should_logout = Prompt.ask("Are you sure you want to logout?", choices=["y", "n"])
+
+    if should_logout == "y":
+        org_name = config.read_config().org_name
+
+        if org_name is not None:
+            delete_password(config.APP_NAME, org_name)
+
+
+app()
