979832ef4ce98c0a3d11e27c68e088d437d31043
Alexander Terekhov <alxtrkhv@gmail.com>
2023-04-09 10:49:40 +0300

    fix: creation of config json

--- gpt_wrapper/config.py
+++ gpt_wrapper/config.py
@@ -1,4 +1,3 @@
-from os import path, makedirs
 from json import load, dump
 from pathlib import Path
 
@@ -39,7 +38,7 @@ def update_config(updated_data: dict | Config) -> bool:
     updated_config = Config(**current_config_dict)
 
     try:
-        with open(FILE_PATH, "a+") as file:
+        with open(FILE_PATH, "w") as file:
             dump(updated_config.dict(), file, indent=2)
     except Exception as e:
         print(f"Error updating config file '{FILE_PATH}': {e}")
@@ -48,7 +47,8 @@ def update_config(updated_data: dict | Config) -> bool:
     return True
 
 
-def _ensure_directory_exists(file_path: str) -> None:
-    dir_name = path.dirname(file_path)
-    if dir_name and not path.exists(dir_name):
-        makedirs(dir_name)
+def _ensure_directory_exists(file_path: Path) -> None:
+    if file_path.exists() is False:
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.touch()
+        file_path.write_text("{}")
