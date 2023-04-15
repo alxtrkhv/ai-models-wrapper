b3129cec7f07f5807914d062a1aa8634b91f91d9
Alexander Terekhov <alxtrkhv@gmail.com>
2023-04-15 09:16:27 +0300

    refactor: avoid implicit dependencies in chat

--- src/chat/chat.py
+++ src/chat/chat.py
@@ -1,8 +1,7 @@
 from enum import StrEnum
-from typing import Callable
+from typing import Callable, Generator, Any
 
 from ..config import read_config
-from ..openai.api import get_api
 
 
 class MessageRole(StrEnum):
@@ -15,6 +14,7 @@ def conversation(
     messages: list[dict],
     system_message_call: Callable[..., str],
     user_message_call: Callable[..., str],
+    completions_call: Callable[[list], Any],
 ):
     system_message = system_message_call()
     if system_message:
@@ -27,19 +27,17 @@ def conversation(
 
         messages.append(message(MessageRole.USER, user_message))
 
-        completion = contextful_completion(messages)
+        completion = completions_call(messages)
         messages.append(completion.choices[0].message)  # type: ignore
 
         yield (completion, user_message)
 
 
-def contextless_completion(input: str):
-    return contextful_completion([message(MessageRole.USER, input)])
+def contextless_completion(input: str, api):
+    return contextful_completion([message(MessageRole.USER, input)], api)
 
 
-def contextful_completion(messages: list):
-    api = get_api()
-
+def contextful_completion(messages: list, api):
     if api is None:
         return
 
--- src/chat/commands.py
+++ src/chat/commands.py
@@ -1,32 +1,42 @@
 from typer import Typer
 
 from .models import Chat
-from .chat import contextless_completion, conversation
+from .chat import contextless_completion, conversation, contextful_completion
 from .view import (
     completion_output,
     system_message_prompt,
     user_message_prompt,
     save_prompt,
-    file_list
+    file_list,
 )
 from ..storage import save, list_files
+from ..openai.api import get_api
 
 chat_app = Typer(name="chat")
 
 
 @chat_app.command()
 def ask(prompt: str):
-    completion_output(contextless_completion(prompt), prompt)
+    api = get_api()
+    if api is None:
+        return
+
+    completion_output(contextless_completion(prompt, api), prompt)
 
 
 @chat_app.command()
 def new():
+    api = get_api()
+    if api is None:
+        return
+
     chat = Chat()
 
     for completion, prompt in conversation(
         messages=chat.messages,
         system_message_call=system_message_prompt,
         user_message_call=user_message_prompt,
+        completions_call=lambda messages: contextful_completion(messages, api),
     ):
         completion_output(completion, prompt)
 
@@ -34,6 +44,7 @@ def new():
 
     if save_prompt():
         save(chat, str(chat.started_at))
+
 
 @chat_app.command()
 def list():
